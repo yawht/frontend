83de290617233354e3c185e57663ceea383e74d6
doritosxxx <georgyvav@ya.ru>
2024-06-11 09:37:02 +0300

    WIP

--- src/pages/CreateGenerationPage/CreateGenerationPage.tsx
+++ src/pages/CreateGenerationPage/CreateGenerationPage.tsx
@@ -1,15 +1,60 @@
 import React from "react";
-import { Box, Button } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import { useMutation } from "@tanstack/react-query";
+import { Box, Button, CircularProgress, FormControl, TextField } from "@mui/material";
 
 import { Page } from "../../components/Page/Page";
 import { ImageUpload } from "../../components/ImageUpload/ImageUpload";
+import { delay } from "../../lib/promise";
+
+interface StartButtonProps {
+    pending?: boolean;
+    onClick?: VoidFunction;
+}
+
+export const StartButton: React.FC<StartButtonProps> = ({ pending, onClick }) => {
+    return (
+        <Button
+            variant="contained"
+            disabled={pending}
+            sx={{ marginTop: '0.8rem' }}
+            onClick={pending ? undefined : onClick}
+            endIcon={pending ? <CircularProgress size={16} /> : undefined}
+        >Сгенерировать!</Button>
+    );
+};
 
 export const CreateGenerationPage: React.FC = () => {
+    const { mutateAsync, isPending } = useMutation<API.GenerationStartResult>({
+        mutationKey: ['generate'],
+        mutationFn: () => {
+            return delay(5000).then(() => ({
+                uid: "some-uuid"
+            }));
+        }
+    });
+
+    const navigate = useNavigate();
+
+    const startGeneration = React.useCallback(() => {
+        return mutateAsync()
+            .then(({ uid }) => navigate(`/generation/${uid}`))
+            .catch(console.error);
+    }, [mutateAsync, navigate]);
+
     return (
         <Page>
-            <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+            <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }} padding="1.6rem">
                 <ImageUpload />
-                <Button variant="contained" sx={{ marginTop: '0.8rem' }}>Сгенерировать!</Button>
+                <FormControl sx={{ marginTop: '1.6rem', width: '51.2rem' }} disabled={isPending}>
+                    <TextField
+                        multiline
+                        variant="outlined"
+                        id="prompt"
+                        label="Input prompt"
+                    />
+                </FormControl>
+                <StartButton pending={isPending} onClick={startGeneration} />
             </Box>
         </Page>
     );
--- src/pages/GenerationPage/GenerationPage.tsx
+++ src/pages/GenerationPage/GenerationPage.tsx
@@ -2,12 +2,51 @@ import React from "react";
 import { useParams } from 'react-router-dom';
 
 import { Page } from "../../components/Page/Page";
+import { ImageList, ImageListItem } from "@mui/material";
+import { useQuery } from "@tanstack/react-query";
+import { delay } from "../../lib/promise";
+
+const RUBY = 'https://cdn.discordapp.com/attachments/497421344644923402/1249438647183474750/S6y4NQcxVKA.jpg?ex=66674dea&is=6665fc6a&hm=948750d52338a90be2a1bb5c471a9941e515b84cf55d4f177224d250755dc0e8&';
 
 export const GenerationPage: React.FC = () => {
-    const { id } = useParams<{ id: string }>();
+    const uid = useParams<{ id: string }>().id || '';
+
+    const { data } = useQuery({
+        queryKey: ['generation', uid],
+        queryFn: () => {
+            return delay(3000).then<API.Generation>(() => ({
+                status: 'in_progress',
+                uid: uid,
+                metadata: {},
+                input_image_link: RUBY,
+                started_at: new Date().toISOString(),
+                input_prompt: "asdasd",
+                results: [{
+                    uid: "1",
+                    image_link: RUBY,
+                    finished_at: new Date().toISOString(),
+                    started_at: new Date().toISOString(),
+                }]
+            }))
+        },
+        initialData: undefined,
+    });
+
     return (
         <Page>
-            {'generation/' + id}
+            {data && <ImageList cols={2} rowHeight='auto' gap={4}>
+                {data.results.map((result) => (
+                    <ImageListItem key={result.uid}>
+                        {result.image_link && (
+                            <img
+                                srcSet={`${result.image_link}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+                                src={`${result.image_link}?w=164&h=164&fit=crop&auto=format`}
+                                loading="lazy"
+                            />
+                        )}
+                    </ImageListItem>
+                ))}
+            </ImageList>}
         </Page>
     );
-}
+};
