dd3c183dce1124d74526b751c72666eaef160295
doritosxxx <georgyvav@ya.ru>
2024-06-11 22:09:41 +0300

    Update typings

--- src/types/api.d.ts
+++ src/types/api.d.ts
@@ -13,17 +13,28 @@ declare namespace API {
     interface Generation {
         uid: string;
         status: Status;
-        started_at: string;
+        started_at: string | null;
         finished_at?: string;
         metadata: Record<string, string>;
 
         input_image_link: string;
-        input_prompt?: string;
+        input_prompt: string | null;
 
         results: GenerationResult[];
     }
 
-    interface GenerationStartResult {
-        uid: string;
+    interface ValidationError {
+        detail: Array<{
+            loc: (number | string)[];
+            msg: string;
+            type: string;
+        }>
+    }
+}
+
+declare namespace POST {
+    interface LaunchGeneration {
+        input_image: string;
+        input_prompt: string | null;
     }
 }
