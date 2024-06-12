45f4a7d991fd883f7b117ce78739436cad351d2a
doritosxxx <georgyvav@ya.ru>
2024-06-12 23:12:39 +0300

    лендос

--- src/pages/LandingPage/LandingPage.tsx
+++ src/pages/LandingPage/LandingPage.tsx
@@ -1,29 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { Button, Stack, Typography } from "@mui/material";
 
 import { Page } from "../../components/Page/Page";
-import { useTheme } from "../../components/Theme";
-
-import './App.css';
 
 export const LandingPage: React.FC = () => {
-    const [theme, toggleTheme] = useTheme();
 
     return (
         <Page>
-            <div id="landing_root">
-                <h1>Vite + React</h1>
-                <div className="card">
-                    <button onClick={toggleTheme}>
-                        theme is {theme}
-                    </button>
-                    <p>
-                        Edit <code>src/App.tsx</code> and save to test HMR
-                    </p>
-                </div>
-                <p className="read-the-docs">
-                    Click on the Vite and React logos to learn more
-                </p>
-            </div>
+            <Stack direction="column" alignItems='center' >
+                <Typography variant="h1" mt="32.0rem">YAHT</Typography>
+                <Link to="/generation">
+                    <Button
+                        variant="contained"
+                        sx={{ marginTop: '1.6rem' }}
+                    >Попробовать</Button>
+                </Link>
+            </Stack>
         </Page>
     );
 };
